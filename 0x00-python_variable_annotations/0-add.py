3d555fa56271d2acd02fefae5f73f9aca2b729ab
abbeypentious@gmail.com <abbeypentious@gmail.com>
2024-10-10 21:59:24 -0700

    variable anotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+    Basic annotations for variables.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Returns sum of two floats.
+    '''
+    return a + b
+
