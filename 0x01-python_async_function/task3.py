e9a47d81ee0db3af136bd81536ad95309b2310d5
abbeypentious@gmail.com <abbeypentious@gmail.com>
2024-10-18 09:16:42 -0700

    update

--- 0x01-python_async_function/task3.py
+++ 0x01-python_async_function/task3.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-""" Async basics """
-
-from asyncio import Task, create_task
-
+"""
+A module for creating asyncio Tasks that run asynchronous functions.
+"""
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Task:
-    """ Tasks """
-    task = create_task(wait_random(max_delay))
-    return task
-
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Create an asyncio Task that waits for a random amount of time
+    up to `max_delay` seconds when run.
+    Returns the created Task object.
+    """
+    return asyncio.create_task(wait_random(max_delay))
--- 0x01-python_async_function/task4.py
+++ 0x01-python_async_function/task4.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-""" The basics of async """
-
+"""
+A module for creating and running multiple asyncio Tasks concurrently.
+"""
 import asyncio
 from typing import List
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    spawn task_wait_random n times with the specified max_delay.
+    Create and run `n` asyncio Tasks that wait for random amounts of time
+    up to `max_delay` seconds. Returns a list of the Task objects, sorted
+    in ascending order by the time waited.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
-
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
